e4066f75701a3c6e4d14c31e602058d1984eb71f
Jim <jimdab@mac.com>
2021-11-10 12:46:45 -0700

    Initial Commit

---
+++ Time of Possession/Time_of_PossessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_of_PossessionApp.swift
+//  Time of Possession
+//
+//  Created by Jim Dabrowski on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Time_of_PossessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
